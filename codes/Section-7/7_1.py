ebcbdc9a3ea47744064f40ce731f2ab912ea9c43
yash rajput <bm20btech11018@iith.ac.in>
2022-08-23 16:29:18 +0530

    assignment 1 done

--- codes/Section-7/7_1.py
+++ codes/Section-7/7_1.py
@@ -1,22 +1,43 @@
 import soundfile as sf
-from scipy import signal
-#read .wav file
-input_signal,fs = sf.read('Sound_Noise.wav')
+from scipy import signal, fft
+import numpy as np
+from numpy.polynomial import Polynomial as P
+from matplotlib import pyplot as plt
+
+def myfiltfilt(b, a, input_signal):
+    X = fft.fft(input_signal)
+    w = np.linspace(0, 1, len(X) + 1)
+    W = np.exp(2j*np.pi*w[:-1])
+    B = (np.absolute(np.polyval(b,W)))**2
+    A = (np.absolute(np.polyval(a,W)))**2
+    Y = B*(1/A)*X
+    return fft.ifft(Y).real
+
+#read .wav file 
+input_signal,fs = sf.read('Sound_Noise.wav') 
+
 #sampling frequency of Input signal
 sampl_freq=fs
-print("Smaple Frequncy is", fs)
+
 #order of the filter
-order=4
+order=4   
+
 #cutoff frquency 4kHz
-cutoff_freq=4000.0
+cutoff_freq=4000.0  
+
 #digital frequency
-Wn=2*cutoff_freq/sampl_freq
+Wn=2*cutoff_freq/sampl_freq  
+
 # b and a are numerator and denominator polynomials respectively
-b, a = signal.butter(order,Wn, 'low')
+b, a = signal.butter(order, Wn, 'low') 
+
 #filter the input signal with butterworth filter
-#output_signal = signal.filtfilt(b, a,input_signal)
-output_signal = signal.lfilter(b, a,input_signal)
-#write the output signal into .wav file
-sf.write('Sound With ReducedNoise.wav',output_signal, fs)
-print("The value of b is",b)
-print("The value of a is", a)
+output_signal = signal.filtfilt(b, a, input_signal)
+#output_signal1 = signal.lfilter(b, a, input_signal)
+os1 = myfiltfilt(b, a, input_signal)
+x_plt = np.arange(len(input_signal))
+#Verify outputs by plotting
+plt.plot(x_plt[:100], output_signal[:100], 'b.')
+plt.plot(x_plt[:100], os1[:100], 'r.')
+plt.grid()
+plt.show()
